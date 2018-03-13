b0db4f8bcbc793b6eb66adf159b127d415f86942
Hideaki Tai <hideaki.tai@gmail.com>
2018-03-13 21:51:29 +0900

    fix include guard

--- Choreograph.h
+++ Choreograph.h
@@ -1,15 +1,37 @@
 #pragma once
+#ifndef CHOREOGRAPH_H
+#define CHOREOGRAPH_H
 
 #ifdef min
+#define min_tmp min
 #undef min
 #endif
 
 #ifdef max
+#define max_tmp max
 #undef max
 #endif
 
 #ifdef abs
+#define abs_tmp abs
 #undef abs
 #endif
 
 #include "lib/src/choreograph/Choreograph.h"
+
+#ifdef min_tmp
+#define min min_tmp
+#undef min_tmp
+#endif
+
+#ifdef max_tmp
+#define max max_tmp
+#undef max_tmp
+#endif
+
+#ifdef abs_tmp
+#define abs abs_tmp
+#undef abs_tmp
+#endif
+
+#endif
